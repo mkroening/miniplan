f1df08ee189550ffd5e443c67481136da4f93ce7
thriqon <github@jonasw.de>
2013-12-02 07:50:08 +0100

    Einteilung-Display working

--- cdn/app/scripts/controllers/gottesdienst_controller.js
+++ cdn/app/scripts/controllers/gottesdienst_controller.js
@@ -3,19 +3,17 @@ App.GottesdienstController = Ember.ObjectController.extend({
 //	splittedEinteilungen : [],
 	splittedEinteilungen : function () {
 		var einteilungen = this.get('model.einteilungen');
-		console.log(einteilungen);
-		return einteilungen;
 
 		var res = [];
 		var lastO = null;
 		einteilungen.forEach(function (item, index) {
 			if (index %2 == 0) {
-				lastO = {links: item};
+				lastO = Ember.Object.create({links: item});
 				res.push(lastO);
 			} else {
 				lastO.rechts = item;
 			}
 		});
-		return res;
-	}.property('einteilungen')
+		return Ember.A(res);
+	}.property('einteilungen.@each')
 });
